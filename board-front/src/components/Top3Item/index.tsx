f01230d42408b5759c54a80d7ac9e93dbdfcbe1c
emptyboy84 <92681205+emptyboy84@users.noreply.github.com>
2024-01-05 17:44:48 +0900

    wtf

--- board-front/src/components/Top3Item/index.tsx
+++ board-front/src/components/Top3Item/index.tsx
@@ -1,29 +1,42 @@
+
+import { BoardListItem } from 'types/interface';
 import './style.css';
+
+interface Props{
+    top3ListItem:BoardListItem;
+
+}
+
 
 
 //component
-export default function Top3Item(){
-    
+export default function Top3Item({top3ListItem}:Props){
+    //properties
+    const {boardNumber,title,content,boardTitleImage}=top3ListItem;
+    const {favoriteCount,commentCount,viewCount}=top3ListItem;
+    const{writeDateTime,writerNickname, writerProfileImage}=top3ListItem;
+
     //rendering
     return(
-         <div className='top-3-list-item'>
+         <div className='top-3-list-item' style={{backgroundImage:`url(${boardTitleImage})`}}>
             <div className='top-3-list-item-main-box'>
                 <div className='top-3-list-item-top'>
                     <div className='top-3-list-item-profile-box'>
-                        <div className='top-3-list-item-profile-image' style={{backgroundImage:`url()`}}></div>
+                        <div className='top-3-list-item-profile-image' style={{backgroundImage:`url(${writerProfileImage})`}}></div>
+
                     </div>
                     <div className='top-3-list-item-write-box'>
-                        <div className='top-3-list-item-nickname'>{'안녕하세요 나는배현명'}</div>
-                        <div className='top-3-list-item-write-date'>{'2024.01.04'}</div>
+                        <div className='top-3-list-item-nickname'>{writerNickname}</div>
+                        <div className='top-3-list-item-write-date'>{writeDateTime}</div>
                     </div>
                 </div>
                 <div className='top-3-list-item-middle'>
-                    <div className='top-3-list-item-title'>{'점심이먼고'}</div>
-                    <div className='top-3-list-item-content'>{'몰라'}</div>
+                    <div className='top-3-list-item-title'>{title}</div>
+                    <div className='top-3-list-item-content'>{content}</div>
                 </div>
                 <div className='top-3-list-item-bottom'>
                     <div className='top-3-list-item-counts'>
-                    {`댓글0 좋아요0 조회수0`}
+                    {`댓글0 좋아요${favoriteCount} 조회수${viewCount}`}
                     </div>
 
                     
